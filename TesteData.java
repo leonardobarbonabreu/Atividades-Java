f046d0cbe12fa27b3f4163387b2016c0870083fe
leonardobarbonabreu <136934997+leonardobarbonabreu@users.noreply.github.com>
2024-06-11 19:12:59 -0300

    Update TesteData.java

--- TesteData.java
+++ TesteData.java
@@ -1,9 +1,31 @@
+import java.util.Scanner;
 
 public class TesteData {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Data data = new Data();	
+		Scanner sc = new Scanner(System.in);
+		int diaSc = 0, mesSc, anoSc, dia = 0, mes =0, ano = 0;
+		
+		
+		
+		
+		System.out.println("Informe um dia: ");
+		diaSc = sc.nextInt();
+		dia = data.getDia(diaSc);
+		
+		System.out.println("Informe um mes: ");
+		mesSc  = sc.nextInt();
+		mes = data.getMes(mesSc);
+		
+		System.out.println("Informe um ano: ");
+		anoSc  = sc.nextInt();
+		ano = data.getAno(anoSc);
+		
+		String conteudo = data.toUniversalString(dia, mes, ano);
+		System.out.println(conteudo);
+		
 	}
 
 }
