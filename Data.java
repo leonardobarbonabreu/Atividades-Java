490202ca4202fddaac80ba91191264de0a00d875
leonardobarbonabreu <136934997+leonardobarbonabreu@users.noreply.github.com>
2024-06-11 19:12:22 -0300

    Update Data.java

--- Data.java
+++ Data.java
@@ -4,7 +4,7 @@ public class Data {
 	private int mes;
 	private int ano;
 	
-	public void setDate(int dia, int mes, int ano) {
+	public void setData(int dia, int mes, int ano) {
 		if (dia < 0 || dia > 31 || mes < 0 || mes > 12  ||ano < 0){
 		  throw new IllegalArgumentException("dia, mês ou ano incorretos"); 
 		}
@@ -13,19 +13,53 @@ public class Data {
 		this.mes = mes;
 		this.ano = ano;
 	}
-	
-	public Data(int dia) {
-		if (dia < 0 || dia >= 31) {
+	///////////////////////////////////////////////////////////////////////////////////////////////////
+	// MUDANÇAS
+	///////////////////////////////////////////////////////////////////////////////////////////////////
+	// Métodos get
+    // obtém valor do dia
+    public int getDia(int dia){
+    	if (dia < 0 || dia >= 31) {
 			throw new IllegalArgumentException("dia tem que estar entre 1-31");
 		}
+    	return dia;
+    }
+    //obtém o valor do mes
+    public int getMes(int mes){
+    	if (dia < 0 || dia >= 31) {
+			throw new IllegalArgumentException("dia tem que estar entre 1-31");
+		}
+    	return mes;
+    }
+    //obtém o valor do ano
+    public int getAno(int ano){
+    	if (dia < 0 || dia >= 31 || mes < 0 || mes > 12) {
+			throw new IllegalArgumentException("dia tem que estar entre 1-31");
+		}
+    	return ano;
+    }
+    
+    // converte em String no formato de data universal (dd/m/aaaa)
+    public String toUniversalString(int dia, int mes, int ano){
+     return String.format("%02d/%02d/%02d", dia, mes, ano);
+    }
+    
+    // converte em String no formato padrão de data/hora (H:MM:SS AM ou PM)
+//    public String toString(){
+//     return String.format("%d/%02d/%02d %s",
+//     ((getDia() == 0 || getDia() == 31 || (getMes() == 2 && getDia() == 29))) ? 12 : getHour() % 12),getMinute(), getSecond(), (getHour() < 12 ? "AM" : "PM"));
+	
+
+	//////////////////////////////////////////////////////////////////////////////////////////////////
+	//CASO SOMENTE UM INPUT SEJA DADO
+	public Data(int dia) {
+		
 		
 		this.dia = dia;
 	}
 	
 	public Data(int dia, int mes) {
-		if (dia < 0 || dia >= 31 || mes < 0 || mes > 12) {
-			throw new IllegalArgumentException("dia tem que estar entre 1-31");
-		}
+		
 		
 		this.mes = mes;
 		this.dia = dia;	
